abace1233b4ae985897d90cd8d45db4825b2133d
vzhz <vehanus@gmail.com>
2018-05-30 18:39:59 -0400

    incomplete interview question answers - come back to this

---
+++ 109_113_Interview_Practice/113_PROJECT_FE_Interview_Questions.js
@@ -0,0 +1,82 @@
+/*
+Notes to reviewer:
+Full-Stack Web Developer DevOps Software Engineer Python Agile Trading
+Job Description
+Full-Stack Web Developer (Software Engineer Python Apache Tom Cat IIS DevOps ChatOps Microservices CI CD Bamboo BitBucket ClojureScript Docker Chef Jenkins Agile Digital Trading Banking) required by our trading software client in New York City, New York. 
+
+You MUST have the following:
+
+Good experience as a full-stack Software Engineer/Developer for web applications; this can be any language including .NET, Java, PHP, C++, Python
+HTML 5, CSS 3, JavaScript for Front End development
+An interest in learning Python
+Web Servers such as IIS, Tom Cat or Apache
+Agile
+The following would be DESIRABLE, not essential:
+
+BitBucket
+Microservices or Domain Driven Design (DDD)
+ClojureScript
+Docker
+ChatOps
+Contribution to the open-source community- GitHub, Stack Overflow
+Continuous integration (Bamboo/Hudson, TeamCity, TFS, MSBuild)
+Automated deployment (Chef, Ansible, Octopus)
+Configuration management (Puppet, PowerShell DSC)
+Role:
+
+Full-Stack Web Developer/Software Engineer required by my trading software client in New York City, New York. You will join a small Agile team of five developers, spread over the US and Europe, that are extending and improving credit and counterparty risk applications. There will be the continuous development of new features in order to incorporate the constant release of financial regulation into the product suite. The suite is web based, built in Python and running on Apache, Tom Cat and MySQL. Although this role will be exclusively developing in Python, Python experience is not required. You can have experience in .NET, Java, PHP, C++ or other languages as long as you are happy to work with Python and have web development experience.
+
+In order to incorporate new financial regulation, the team adopts a highly Agile DevOps environment. This results in several releases a day with the use of Bamboo, BitBucket and Confluence for continuous integration, deployment and source control.
+
+The environment is modern and progressive. There will be excellent opportunities to progress in to Lead Developer and Architect roles.
+
+Salary: $80k - £100k + Bonus + Benefits
+*/
+
+/*
+BEHAVIORAL QUESTIONS
+*/
+
+
+/*
+1. What is the most influential book or blog post you’ve read regarding front-end development?
+To me, the most influential book/post is the one that is the most clear and usable a resource. 
+For me, that is the tounge-in-cheek guide to Javascript "Ugh, Code. I read it as my first look 
+at Javascript and, while the details of the project I completed have faded, I continue to appreciate
+the authors scoping skills and ability to share the right level of detail with his students. This 
+flexibility is a skill I try to emulate whenever I am in a position to explain something to others or
+need to adjust my own process.
+*/
+
+/*
+If you could master one technology this year, what would it be and why?
+I would like to learn more about Python's data analysis libraries. As I grow as a web developer, I
+expect that I will become more intregrated into my team's decision-making, and I want to be able
+to base my recommendations on the data we have from our users and be able to analyze that effectively.
+*/
+
+
+/*
+Describe any front-end web application framework (preferably one that you use). How does it work? What are the upsides and downsides?
+It's important to remember that React was built as a JS library for building user interfaces. Because
+React isn't a full framework, some tasks are left to 3rd parties (you get to choose!). This means that 
+there is far more choice than a lot if beginners are ready for and best practice can be murky. 
+However, because it is so popular there are an abundence of training resources. It's a good choice (note to self: why?)
+for cross-platform work. Because your view is rerendered but uses declaritive components, it's performant. No explicit 
+data binding means that you don't have to wire your view to your model manually and instead uses one render function
+which returns your view and calls it recursively.
+(Girl, this is word salad, you need to go back and learn more.)
+*/
+
+/*
+Write a JavaScript function that takes only one argument——another function——and returns a "memoized" version of that function. A "memoized" version of a function caches and returns the results of its call so that when it is called again with the same input, it doesn’t run its computation but instead returns the results from cache. Note that previous results should be retrievable in any order without re-computation.
+
+Come back and finish this after you learn more. :p
+
+*/
+
+/*
+
+
+*/
+
